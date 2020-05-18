0ba17f01378e3a4b5671955b4707cdd56f8442cf
Muhamad Adam 84 <muhamadadam20@gmail.com>
2020-05-18 09:42:25 +0700

    change auth

--- src/routes/author.js
+++ src/routes/author.js
@@ -4,11 +4,9 @@ const authorController = require('../controllers/author')
 const needAuth = require('../middlewares/need_auth')
 const adminOnly = require('../middlewares/role_admin')
 
-router.use(needAuth)
-
 router.get('/', authorController.get)
-router.post('/', adminOnly, authorController.post)
-router.patch('/:id', adminOnly, authorController.patch)
-router.delete('/:id', adminOnly, authorController.delete)
+router.post('/', needAuth, adminOnly, authorController.post)
+router.patch('/:id', needAuth, adminOnly, authorController.patch)
+router.delete('/:id', needAuth, adminOnly, authorController.delete)
 
 module.exports = router
--- src/routes/genre.js
+++ src/routes/genre.js
@@ -4,11 +4,9 @@ const genreController = require('../controllers/genre')
 const needAuth = require('../middlewares/need_auth')
 const adminOnly = require('../middlewares/role_admin')
 
-router.use(needAuth)
-
 router.get('/', genreController.get)
-router.post('/', adminOnly, genreController.post)
-router.patch('/:id', adminOnly, genreController.patch)
-router.delete('/:id', adminOnly, genreController.delete)
+router.post('/', needAuth, adminOnly, genreController.post)
+router.patch('/:id', needAuth, adminOnly, genreController.patch)
+router.delete('/:id', needAuth, adminOnly, genreController.delete)
 
 module.exports = router
